188b60967cb8b5ad915b0416217e3baacf66d0a8
Paul Offord <paul.offord58@gmail.com>
2024-11-11 14:05:00 +0000

    Code to avoid reset of rpm and speed by unrelated canids

--- backend.py
+++ backend.py
@@ -41,7 +41,7 @@ class Backend:
         return gear_number  # gear number
 
     def get_can_message(self) -> dict:
-        message_content = {'speed': 0, 'rpm': 0}
+        message_content = {'canid': 0, 'speed': 0, 'rpm': 0}
         if shared_memory.settings.get_test_mode():
             row = self.test_data_frame.iloc[self.test_data_index]
             message_content['speed'] = row['SPEED BMW (kph)']
@@ -50,13 +50,15 @@ class Backend:
         else:
             msg = self.bus_vector.recv()
             if msg.arbitration_id == 436:  # 436 (0x1b4) gives speed
+                message_content['canid'] = msg.arbitration_id
                 # (((Byte[1] - 208) * 256) + Byte[0]) / 16 -> gives mph
                 # therefore
                 # (((Byte[1] - 208) * 256) + Byte[0]) / 10 -> gives kph
                 message_content['speed'] = (((int(msg.data[1]) - 208) * 256) + int(msg.data[0])) / 10
                 # print("Speed (kph): ", message_content['speed'])
 
-            elif msg.arbitration_id == 170:  # 170 (0xAA) gives speed
+            elif msg.arbitration_id == 170:  # 170 (0xAA) gives rpm
+                message_content['canid'] = msg.arbitration_id
                 # ((Byte[5] * 256) + Byte[4] ) / 4 -> gives rpm
                 message_content['rpm'] = ((int(msg.data[5]) * 256) + int(msg.data[4])) / 4
                 # print("Revs (rpm): ", message_content['rpm'])
@@ -68,8 +70,10 @@ class Backend:
 
         while shared_memory.run_state == shared_memory.RUN_STATE_RUNNING:
             message_values = self.get_can_message()
-            shared_memory.speed = self.calculate_adjusted_speed(int(message_values['speed']))
-            shared_memory.eng_rpm = int(message_values['rpm'])
+            if message_values['canid'] == 436:
+                shared_memory.speed = self.calculate_adjusted_speed(int(message_values['speed']))
+            elif message_values['canid'] == 170:
+                shared_memory.eng_rpm = int(message_values['rpm'])
             shared_memory.pre_calc_gear = self.calculate_gear(
                 speed=shared_memory.speed,
                 rpm=shared_memory.eng_rpm
