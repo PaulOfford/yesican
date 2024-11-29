87a1344e1979805657a1e4683d1c0c88ecba8e39
Paul Offord <paul.offord58@gmail.com>
2024-11-29 09:33:53 +0000

    Created BrakeTrail class

--- plot_test.py
+++ plot_test.py
@@ -1,98 +1,93 @@
 import tkinter as tk
 from time import sleep
-from tkinter import filedialog
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import numpy as np
 
 import csv
-
-import random
 
 
 # --- functions ---  # PEP8: `lower_case_names`
 
-def open_file():
-    global x
-    global y
+class BrakeTrail:
+    # default values at start
+    x = []
+    y = []
 
-    # file = filedialog.askopenfile(filetypes=[('CSV Files', '*.csv'),
-    #                                          ('Text Files', '*.txt'),
-    #                                          ('All Files', '*.*')])
-
-    file = open('test_data_tb.csv', 'r')
-
-    if file:
-        print('read file')
-
-        x = []
-        y = []
-
-        reader_csv = csv.reader(file)
-        for row in reader_csv:
-            if row[0] == 'Time (s)':
-                continue
-            x.append(float(row[0]))  # time offset
-            # y.append(float(row[1]))
-            y.append(float(row[16]))  # brake pressure
-
-        if x:
-            ax.set_xlim(0, 10)
-        if y:
-            ax.set_ylim(min(y), max(y))
+    fig = None
+    ax = None  # axes object for the plot
+    line = None  # plot line
 
 
-def animate(i):
-    # print('i:', i, x[i:i + 10], y[i:i + 10])
-    sleep(0.05)
+    def build_plot(self, container):
+        self.fig = plt.Figure()
 
-    current_x = x[i:i + 128]
-    current_y = y[i:i + 128]
+        canvas = FigureCanvasTkAgg(self.fig, master=container)
+        canvas.get_tk_widget().pack()
 
-    line.set_xdata(current_x)
-    line.set_ydata(current_y)
-    line.set_color('r')
+        self.ax = self.fig.add_subplot(111)
+        # ax.set_xticks([])
+        self.ax.yaxis.tick_right()
+        self.ax.set_ylim(ymax=140)
+        self.line, = self.ax.plot([], [])
 
-    if current_x:
-        ax.set_xlim(min(current_x), max(current_x))
+    def load_test_data(self):
+        file = open('test_data_tb.csv', 'r')
 
-    # canvas.draw()
+        if file:
+            print('read file')
 
-    return line,
+            reader_csv = csv.reader(file)
+            for row in reader_csv:
+                if row[0] == 'Time (s)':
+                    continue
+                self.x.append(float(row[0]))  # time offset
+                # y.append(float(row[1]))
+                self.y.append(float(row[16]))  # brake pressure
+
+            if self.x:
+                self.ax.set_xlim(0, 10)
+            if self.y:
+                self.ax.set_ylim(min(self.y), max(self.y))
+
+    def animate(self, i):
+        # print('i:', i, x[i:i + 10], y[i:i + 10])
+        sleep(0.01)
+
+        current_x = self.x[i:i + 128]
+        current_y = self.y[i:i + 128]
+
+        self.line.set_xdata(current_x)
+        self.line.set_ydata(current_y)
+        self.line.set_color('r')
+
+        if current_x:
+            self.ax.set_xlim(min(current_x), max(current_x))
+
+        # canvas.draw()
+
+        return self.line,
 
 
-# --- main ---
+    # --- main ---
 
-# default values at start
-x = []
-y = []
+    def run_plot(self):
+        # - GUI -
 
-# - GUI -
+        try:
+            self.ani = animation.FuncAnimation(self.fig, self.animate, 512, interval=0, blit=False)
 
-tk_top = tk.Tk()
+        except NameError as ex:
+            print(ex)
 
-try:
-    fig = plt.Figure()
-
-    canvas = FigureCanvasTkAgg(fig, master=tk_top)
-    canvas.get_tk_widget().pack()
-
-    ax = fig.add_subplot(111)
-    open_file()
-    # ax.set_xticks([])
-    ax.set_ylim(ymax=140)
-    line, = ax.plot([], [])
-
-    # ani = animation.FuncAnimation(fig, animate, 250, interval=250, blit=False)
-    ani = animation.FuncAnimation(fig, animate, 512, interval=0, blit=False)
-    # ani = animation.FuncAnimation(fig, animate, 512, blit=False)
-
-except NameError as ex:
-    print(ex)
-
-print('mainloop')
-tk_top.mainloop()
+if __name__ == "__main__":
+    bt_plot = BrakeTrail()
+    tk_top = tk.Tk()
+    bt_plot.build_plot(tk_top)
+    bt_plot.load_test_data()
+    bt_plot.run_plot()
+    print('mainloop')
+    tk_top.mainloop()
